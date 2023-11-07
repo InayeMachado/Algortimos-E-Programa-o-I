a6d0ef24bf31a7f3c866eaf5f6f74f9fba87b4a8
InayeMachado <104857020+InayeMachado@users.noreply.github.com>
2023-11-07 11:22:26 -0300

    Feito um novo programa e adicionada legendao ao arquivo README.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Algortimos-E-Programa-o-I
+Inayê Machado
 Atividades realizadas nas aulas de Algoritmos e Programação I na UFFS
---
+++ programa.py
@@ -0,0 +1,5 @@
+print("Este é um algortimo onde você pode informar o valor da temperatura em graus Fahrenheit e logo saberá a temperatura em graus Celsius.")
+
+temperatura = float (input("Informe a temperatura em graus Fahrenheit: "))
+celsius = (temperatura - 32)/1.8
+print("A temperatura em graus Celsius é: ", celsius)
